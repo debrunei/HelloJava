f684aed8f882fc7a3545d7be9cef5cb56f37091b
debrunei000 <debrunei000@warren.k12.in.us>
2025-07-29 13:40:56 -0400

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- src/HelloJava.java
+++ src/HelloJava.java
@@ -2,6 +2,16 @@ public class HelloJava {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        String name = "Neil D";
+        System.out.println("Hello my name is " + name);
+        int age = 17;
+        System.out.println("And my age is " + age);
+        String Fun_fact = "I do origami";
+        System.out.println("A fun fact about me is " + Fun_fact);
+
     }
+
+
 
 }
